b14d03f1d27bad73d3d7a4f1ccc7148f43b96e99
Luana Souza <luana.souza@ifood.com.br>
2021-03-06 20:53:19 -0300

    passado parementros para uma activity

--- app/src/main/java/br/com/luanadev/myapplication/MainActivity.kt
+++ app/src/main/java/br/com/luanadev/myapplication/MainActivity.kt
@@ -1,21 +1,31 @@
 package br.com.luanadev.myapplication
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        toast()
+        novaTela()
+    }
+
+    fun toast() {
         buttonToast.setOnClickListener {
             val texto = editTexto.text.toString()
             Toast.makeText(this, "Texto", Toast.LENGTH_SHORT).show()
         }
+    }
+
+    fun novaTela() {
         buttonTela2.setOnClickListener {
             val intent = Intent(this, Tela2Activity::class.java)
+            intent.putExtra("nome", "Luana")
+            intent.putExtra("idade", "32")
             startActivity(intent
             )
         }
--- app/src/main/java/br/com/luanadev/myapplication/Tela2Activity.kt
+++ app/src/main/java/br/com/luanadev/myapplication/Tela2Activity.kt
@@ -2,10 +2,14 @@ package br.com.luanadev.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_tela2.*
 
 class Tela2Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tela2)
+        val nome = intent.getStringExtra("nome")
+        val idade = intent.getStringExtra("idade")
+        textMensagem.text = "Nome: $nome / Idade: $idade"
     }
 }
